2bfe83a5ad09c373dad7b79e325432d2d4e57a27
Ozelitotiktak <odioalpixel@gmail.com>
2016-03-11 11:36:41 +0100

    #1 - Commit inicial

---
+++ src/examen/Calculadora.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen;
+
+/**
+ *
+ * @author Ozelitotiktak
+ */
+public class Calculadora {
+    
+    public int sumar(int uno, int dos) {
+        return uno + dos;
+    }
+    
+    public int restar(int uno, int dos) {
+        return uno - dos;
+    }
+    
+    public int dividir(int uno, int dos) {
+        return uno / dos;
+    }
+    
+    public int multiplicar(int uno, int dos) {
+        return uno * dos;
+    }
+}
